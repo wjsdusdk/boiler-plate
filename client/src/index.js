90bfde37fa9dde819c04c3ab8bdf8f58d46a6719
wjsdusdk <wjsdk1570@gmail.com>
2021-03-30 23:47:41 +0900

    redux

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import "antd/dist/antd.css";
+import { applyMiddleware } from "redux";
+import { promiseMiddleware } from "redux-promise";
+import { ReduxThunk } from "redux-thunk";
+import Reducer from "./_reducers"; // = "./_reducers/index.js"
 
-import 'antd/dist/antd.css';
+const creatStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.render(
-    <React.StrictMode>
+    <Provider store={creatStoreWithMiddleware(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
         <App />
-    </React.StrictMode>,
+    </Provider>,
     document.getElementById("root")
 );
 
